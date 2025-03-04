e03d6ed1c8403a6eabf87a3d135e3ea2e8ed4b6c
IuriyLeb <iuriy.leb@gmail.com>
2025-03-04 23:53:05 +0000

    remote settings upgrade

--- rishat_test/settings.py
+++ rishat_test/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
-
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "localhost").split(",")
 
 # Application definition
 
